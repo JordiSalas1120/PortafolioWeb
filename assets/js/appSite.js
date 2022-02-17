24b4262ab98605db28b3f9dc0820d80261d4430b
JordiSalas1120 <90120002+JordiSalas1120@users.noreply.github.com>
2022-02-17 02:51:49 -0400

    Add files via upload

---
+++ assets/js/appSite.js
@@ -0,0 +1,44 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp();
+});
+
+const links = {
+	1:"https://festivaldemusica-rock-edm.netlify.app/",
+	2:"https://frontendstore-proyecto.netlify.app/",
+	3:"https://jordifreelancer1.netlify.app/",
+	4:"https://juego2d-en-js.netlify.app/",
+	5:"https://youtube.com",
+	6:"https://variables-css.netlify.app/",
+	7:"https://web-pug-leidy.netlify.app/",
+	8:"https://blog-de-recetas-de-cafe.netlify.app/"
+}
+function iniciarApp(){
+    
+    crearGaleria();
+   
+    
+}
+function crearGaleria(){
+    const galeria = document.querySelector('.galeria');
+    var l = 0;
+    for(let i = 1; i <= 8; i++){
+    	
+        let link = Object.values(links);
+        const imagen = document.createElement('picture');
+        imagen.classList.add('contenedor');
+       
+      	imagen.innerHTML =
+        `<a href=${link[l]}> <img class="imagen" loading="lazy" src="assets/sitios/${i}.png" alt="imagen de la galeria">
+        		</a>
+       			`;
+        
+
+        galeria.appendChild(imagen);
+         
+        
+        
+        l = l + 1; 
+    }
+    
+}
