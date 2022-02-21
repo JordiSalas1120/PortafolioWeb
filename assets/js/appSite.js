14eda48d42e97e1b1b85eaba5e7410f7f2a804dc
JordiSalas1120 <90120002+JordiSalas1120@users.noreply.github.com>
2022-02-21 14:00:11 -0400

    Add files via upload

--- assets/js/appSite.js
+++ assets/js/appSite.js
@@ -11,31 +11,28 @@ const links = {
 	5:"https://youtube.com",
 	6:"https://variables-css.netlify.app/",
 	7:"https://web-pug-leidy.netlify.app/",
-	8:"https://blog-de-recetas-de-cafe.netlify.app/"
+	8:"https://blog-de-recetas-de-cafe.netlify.app/",
+    9:"https://calculadora-jordi.netlify.app/"
 }
 function iniciarApp(){
     
     crearGaleria();
-   
     
 }
 function crearGaleria(){
     const galeria = document.querySelector('.galeria');
     var l = 0;
-    for(let i = 1; i <= 8; i++){
+    for(let i = 1; i <= 9; i++){
     	
         let link = Object.values(links);
         const imagen = document.createElement('picture');
         imagen.classList.add('contenedor');
-       
-      	imagen.innerHTML =
+        imagen.innerHTML =
         `<a href=${link[l]}> <img class="imagen" loading="lazy" src="assets/sitios/${i}.png" alt="imagen de la galeria">
-        		</a>
-       			`;
+        </a>`;
         
 
         galeria.appendChild(imagen);
-         
         
         
         l = l + 1; 
